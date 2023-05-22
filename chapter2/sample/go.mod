8ebdc189ccdd8094ed4cc0d8ad6e306c16072acd
haydenwang <wanghaiyan1111@gmail.com>
2023-05-22 11:02:29 +0800

    init()

---
+++ chapter2/sample/go.mod
@@ -0,0 +1,5 @@
+module chapter2.sample
+
+go 1.19
+
+require github.com/goinaction/code v0.0.0-20171020164608-49fc99e6affb // indirect
--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -11,11 +12,29 @@ import (
 // init is called prior to main.
 func init() {
 	// Change the device for logging to stdout.
+	fmt.Println("here is my first init??")
+	log.SetOutput(os.Stdout)
+}
+
+// init is called prior to main.
+func init() {
+	// Change the device for logging to stdout.
+	fmt.Println("here is my init.......")
+	log.SetOutput(os.Stdout)
+}
+
+// init is called prior to main.
+func init() {
+	// Change the device for logging to stdout.
+	fmt.Println("here is my another init.......")
 	log.SetOutput(os.Stdout)
 }
 
 // main is the entry point for the program.
 func main() {
+	fmt.Println("....start from hayden................")
+
 	// Perform the search for the specified term.
 	search.Run("president")
+	fmt.Print("here is the end....................")
 }
